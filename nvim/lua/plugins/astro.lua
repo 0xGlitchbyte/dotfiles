07eeddad4512d561b43d2402d549aaafeab878a7
0xGlitchbyte <49317853+0xGlitchbyte@users.noreply.github.com>
2023-12-31 15:46:31 -0500

    init commit

---
+++ nvim/lua/plugins/astro.lua
@@ -0,0 +1,11 @@
+return {
+  { "wuelnerdotexe/vim-astro" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      astro_typescript = "enable",
+      astro_stylus = "enable",
+    },
+  },
+}
---
+++ nvim/lua/plugins/gruvbox.lua
@@ -0,0 +1,10 @@
+return {
+  { "ellisonleao/gruvbox.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "gruvbox",
+      transparent_mode = true,
+    },
+  },
+}
